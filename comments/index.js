f5e2739a23d89420e410790478fda7c7a8daea4f
Mohammed Abuelwafa <mohammedabuelwafa@aucegypt.edu>
2024-10-11 22:03:04 +0300

    Adds comment moderation feature for the word "orange"

--- comments/index.js
+++ comments/index.js
@@ -25,7 +25,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
     commentsByPostId[req.params.id] = comments;
 
     await axios.post("http://localhost:4005/events",
@@ -34,6 +34,7 @@ app.post('/posts/:id/comments', async (req, res) => {
             data: {
                 id: commentId,
                 content,
+                status: 'pending',
                 postId: req.params.id
             }
         },)
@@ -44,9 +45,35 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
 
     console.log('Received Event', req.body.type);
+
+    const { type, data } = req.body;
+    if (type == 'CommentModerated') {
+        const { postId, id, status } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => comment.id === id);
+        comment.status = status;
+
+        await axios.post("http://localhost:4005/events",
+            {
+                type: 'CommentUpdated',
+                data: {
+                    id,
+                    content,
+                    status,
+                    postId
+                }
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+
+    }
+
+
     res.send({});
 
 });
--- moderation/index.js
+++ moderation/index.js
@@ -9,9 +9,30 @@ app.use(bodyParser.json());
 app.post('/events', async (req, res) => {
     const { type, data } = req.body;
 
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
 
-    res.send({});
+        await axios.post("http://localhost:4003/events",
+            {
+                type: 'CommentModerated',
+                data: {
+                    id: data.id,
+                    postId: data.postId,
+                    status,
+                    content: data.content
+                }
+
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+
+        res.send({});
+
+    }
+
+
 });
 
 app.listen(4003, () => console.log("Listening on 4003"));
